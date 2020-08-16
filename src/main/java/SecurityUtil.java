4ef20051bd523f99737744581ea1fb31f6de1ed0
goni9071 <goni9071@nate.com>
2020-08-16 18:39:27 +0900

    SecurityUtil - getRandomAlphaNumeric 추가

--- src/main/java/SecurityUtil.java
+++ src/main/java/SecurityUtil.java
@@ -45,4 +45,19 @@ public class SecurityUtil {
     }
     return retValue;
   }
+
+  public static String getRandomAlphaNumeric(int len) {
+    char[] charSet = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+        'v', 'w', 'x', 'y', 'z' };
+
+    int idx = 0;
+    StringBuffer sb = new StringBuffer();
+
+    for (int i = 0; i < len; i++) {
+      idx = (int) (charSet.length * Math.random());
+      sb.append(charSet[idx]);
+    }
+
+    return sb.toString();
+  }
 }
